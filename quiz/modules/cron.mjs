e4ffa71d2f8087d1c6956673d58fb43f30c1927c
LUIZ-FELIPE3393 <2023003393@ifam.edu.br>
2024-08-30 14:06:39 -0400

    modified:   quiz/modules/cron.mjs 	modified:   quiz/quiz.js

--- quiz/modules/cron.mjs
+++ quiz/modules/cron.mjs
@@ -1,4 +1,4 @@
-export class Cron {
+class Cron {
     constructor(second, minute, hour) {
         this.second = second;
         this.minute = minute;
--- quiz/quiz.js
+++ quiz/quiz.js
@@ -1,4 +1,41 @@
-import { Cron } from "./modules/cron.mjs";
+class Cron {
+    constructor(second, minute, hour) {
+        this.second = second;
+        this.minute = minute;
+        this.hour = hour;
+    }
+
+    start() {
+        this.timerInterval = setInterval(() => {this.timer();}, 1000)
+    }
+
+    stop() {
+        clearInterval(this.timerInterval);
+    }
+
+    reset() {
+        this.second = 0;
+        this.minute = 0;
+        this.hour = 0;
+    }
+
+    timer() {
+        if ((this.second += 1) == 60) {
+            this.second = 0
+            this.minute += 1;
+        }
+        if (this.minute == 60) {
+            this.minute = 0
+            this.hour += 1;
+        }
+
+        document.querySelector("#cron-text").innerText = 
+            ("00" + this.hour).slice(-2) + ":" +
+           ("00" + this.minute).slice(-2) + 
+            ":" + ("00" + this.second).slice(-2);
+    }
+}
+
 
 const answerBlock = document.querySelector("#answerBlock");
 const questionBlock = document.querySelector("#questionBlock");
